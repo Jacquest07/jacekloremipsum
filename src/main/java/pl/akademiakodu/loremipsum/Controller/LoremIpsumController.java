764ede34affe0b2c3201494bd436c521682729db
Jacek Raczynski <jacquest@tlen.pl>
2017-09-01 15:21:19 +0200

    checking home

---
+++ src/main/java/pl/akademiakodu/loremipsum/Controller/LoremIpsumController.java
@@ -0,0 +1,8 @@
+package pl.akademiakodu.loremipsum.Controller;
+
+/**
+ * Created by Eriot on 2017-09-01.
+ */
+public class LoremIpsumController
+{
+}
---
+++ src/main/java/pl/akademiakodu/loremipsum/LoremIpsum.java
@@ -0,0 +1,8 @@
+package pl.akademiakodu.loremipsum;
+
+/**
+ * Created by Eriot on 2017-09-01.
+ */
+public interface LoremIpsum
+{
+}
